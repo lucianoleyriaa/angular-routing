5ae1cd8a9209252caac7e516a5acc502ab2b8c59
Luciano <lucholeyria@gmail.com>
2021-06-03 07:16:32 -0300

    =Se agrego el componente not-found y se configuraron rutas para renderizarlos cuando se quiera acceder a rutas que no existan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { UserComponent } from './users/user/user.component';
 import { ServersService } from './servers/servers.service';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   // path: => indica la ruta y component => indica el componente que se a renderizar cuando se ingrese a esa ruta
@@ -34,6 +35,8 @@ const routes: Routes = [
       { path: ':id/edit', component: EditServerComponent },
     ],
   },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
 
 @NgModule({
@@ -45,6 +48,7 @@ const routes: Routes = [
     EditServerComponent,
     ServerComponent,
     UserComponent,
+    NotFoundComponent,
   ],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [ServersService],
